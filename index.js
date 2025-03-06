992263d6def0377cbb1dbaa4ff397a1310a34280
Samuel Fernandes <58116807+Samuelfdesm@users.noreply.github.com>
2025-03-06 15:02:07 -0300

    Excluindo algumas 'sujeiras' do código

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // const connection = require("./database/database");
 // const Pergunta = require("./database/Pergunta");
-// const Resposta = require("./database/Resposta");
+
 //Database
 
 // Estou dizendo para o Express usar o EJS como View Engine
@@ -16,69 +16,12 @@ app.use(bodyParser.json());
 
 
 //Rotas
+
 app.get("/", (req, res) => {
-    Pergunta.findAll({raw: true, order:[
-        ['id','DESC']
-    ]}).then(perguntas => {
-        res.render("index", {
-            perguntas: perguntas
-        });
-    });
-});
-
-app.get("/perguntar", (req, res) => {
-    res.render("perguntar");
-});
-
-app.post("/salvarpergunta", (req, res) => {
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
-    Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
-    }).then(() => {
-        res.redirect("/");
-    });
+    res.render("index");
 });
 
 
-app.get("/pergunta/:id", (req, res) => {
-    var id = req.params.id;
-    Pergunta.findOne({
-        where: {id: id},
-
-    }).then(pergunta => {
-        if(pergunta != undefined){ // Pergunta encontrada
-
-            Resposta.findAll({
-                where: {perguntaId: pergunta.id},
-                order: [
-                    ['id','DESC']
-                ]
-            }).then(respostas => {
-                res.render("pergunta", {
-                    pergunta: pergunta,
-                    respostas: respostas
-                });
-            });
-
-        }else{ // Não encontrada
-            res.redirect("/");
-        }
-    })
-});
-
-app.post("/responder", (req, res) => {
-    var corpo = req.body.corpo;
-    var perguntaId = req.body.pergunta;
-
-    Resposta.create({
-        corpo: corpo,
-        perguntaId: perguntaId
-    }).then(() => {
-        res.redirect("/pergunta/" + perguntaId);
-    });
-});
 
 app.listen(80, () => {console.log("App rodando!");});
 
