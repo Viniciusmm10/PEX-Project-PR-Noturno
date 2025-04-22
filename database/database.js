b33d310a4e1668524a71cfaec2af6864f9838010
Samuel Fernandes <58116807+Samuelfdesm@users.noreply.github.com>
2025-04-22 13:08:10 -0300

    Criado um usuário exclusivo para o banco de dados

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("quiz", "root", "as,k71l;", {
+const connection = new Sequelize("quiz", "quiz", "yrsZo1XsAFGgZO72gbzK5fuyW9XhKf7pGJqtc03tNlA=", {
     host: "localhost",
     dialect: "mysql"
 });
