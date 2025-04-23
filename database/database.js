dcbf03e4a4d40386913ed70ac6d22a450e68e149
Samuel Fernandes <58116807+Samuelfdesm@users.noreply.github.com>
2025-04-23 11:35:12 -0300

    Alterado o IP do banco de dados para o BD do VPS

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize("quiz", "quiz", "yrsZo1XsAFGgZO72gbzK5fuyW9XhKf7pGJqtc03tNlA=", {
-    host: "localhost",
+    host: "157.230.9.56",
     dialect: "mysql"
 });
 
