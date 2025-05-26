86c41c18c4a6b766d9888f4daaccfcb700bbb37d
Samuel Fernandes <samuelfdesm@gmail.com>
2025-05-25 21:18:38 -0300

    Atualizado as credenciais do banco de dados

--- database/database.js
+++ database/database.js
@@ -1,14 +1,9 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize("quiz", "quiz", "yrsZo1XsAFGgZO72gbzK5fuyW9XhKf7pGJqtc03tNlA=", {
-    host: "159.223.186.117",
+    host: "localhost",
     dialect: "mysql"
 });
-
-// const connection = new Sequelize("quiz", "root", "as,k71l;", {
-//     host: "localhost",
-//     dialect: "mysql"
-// });
 
 
 module.exports = connection;
